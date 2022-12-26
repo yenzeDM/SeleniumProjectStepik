892bfd84d461361dba97549cf80e886fda54556e
Dmitrij <dmitrij.eremeev.99@gmail.com>
2022-12-26 12:38:16 +0300

    Import sleep from time

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import By
+from time import sleep
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -6,3 +7,4 @@ def test_guest_can_go_to_login_page(browser):
     browser.get(link)
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
+    sleep(4)
