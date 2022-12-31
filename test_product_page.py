9771c2701a72d55f2b1940a9284c5f6a9038b52c
Dmitrij <dmitrij.eremeev.99@gmail.com>
2022-12-31 11:02:26 +0300

    base page class is changed and added new tests and main page class's methods were transferred in base page class

--- test_product_page.py
+++ test_product_page.py
@@ -23,32 +23,46 @@ from time import sleep
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_can_add_product_to_basket(browser):
-    product_page = ProductPage(browser, link)
-    product_page.open()
-    product_page.should_be_submit_add_basket()
-    product_page.add_to_basket()
-    product_page.message_coincide_with_name_product()
-    product_page.message_price_coincide_with_price_product()
+# def test_guest_can_add_product_to_basket(browser):
+#     product_page = ProductPage(browser, link)
+#     product_page.open()
+#     product_page.should_be_submit_add_basket()
+#     product_page.add_to_basket()
+#     product_page.message_coincide_with_name_product()
+#     product_page.message_price_coincide_with_price_product()
 
 
-@pytest.mark.xfail
-def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-    product_page = ProductPage(browser, link)
-    product_page.open()
-    product_page.add_to_basket()
-    product_page.should_not_be_success_message()
+# @pytest.mark.xfail
+# def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+#     product_page = ProductPage(browser, link)
+#     product_page.open()
+#     product_page.add_to_basket()
+#     product_page.should_not_be_success_message()
 
 
-def test_guest_cant_see_success_message(browser):
-    product_page = ProductPage(browser, link)
-    product_page.open()
-    product_page.should_not_be_success_message()
+# def test_guest_cant_see_success_message(browser):
+#     product_page = ProductPage(browser, link)
+#     product_page.open()
+#     product_page.should_not_be_success_message()
 
 
-@pytest.mark.xfail
-def test_message_disappeared_after_adding_product_to_basket(browser):
-    product_page = ProductPage(browser, link)
-    product_page.open()
-    product_page.add_to_basket()
-    product_page.success_message_should_disappear()
+# @pytest.mark.xfail
+# def test_message_disappeared_after_adding_product_to_basket(browser):
+#     product_page = ProductPage(browser, link)
+#     product_page.open()
+#     product_page.add_to_basket()
+#     product_page.success_message_should_disappear()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
